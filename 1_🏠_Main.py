db93802a400a07177a1e002abde1fa9860261557
Jakub Pietrucha <elpietruch@gmail.com>
2025-01-04 21:46:27 +0100

    add a lot of stuff 😥

--- 1_🏠_Main.py
+++ 1_🏠_Main.py
@@ -29,8 +29,8 @@ def page_config():
         unsafe_allow_html=True
         )
 
-def myFunc(e):
-    return e.token_fullname
+def sort_tokens_alphabetically(e):
+    return e.token_id
 
 def get_color_for_token(token_name):
     if isinstance(token_name, str):
@@ -45,44 +45,73 @@ def get_color_for_token(token_name):
     if 'MOUNTAIN' in token_name:
         return 'grey'
 
+def set_app_state_to_monster_weakness_tokens_placed():
+    st.session_state.app_state = 'monster_weakness_tokens_placed'
+
+def check_number_of_tokens_left_in_bag(remove_type, remove_territory_type):
+    if 'WEAKNESS' in remove_type:
+        return sum(1 for i in tokens.WEAKNESS_TOKENS_BAG if i.territory_type == remove_territory_type)
+    else:
+        return sum(1 for i in tokens.TRAIL_TOKENS_BAG if i.territory_type == remove_territory_type)
+
 if __name__ == "__main__":
+    page_config()
     print("*** ENTIRELY NEW RUN ***")
     session_state = st.session_state
 
-# THIS IS INITIAL SETUP AND NEED TO BE DONE ONLY ONCE
+#? THIS IS INITIAL SETUP AND NEED TO BE DONE ONLY ONCE
     if 'app_state' not in session_state:
-        session_state.app_state = 'initial_setup_started'  
-        session_state.counter = 0
+        st.session_state.app_state = 'initial_setup_started'  
+        st.session_state.counter = 0
 
-    st.write(f":orange[session_state]: {st.session_state.app_state}")
+    if 'most_recently_chosen_token' not in st.session_state:
+        st.session_state['most_recently_chosen_token'] = 'None'
+
+    # st.write(f":orange[session_state]: {st.session_state.app_state}")
+    # st.write(f":orange[session_state]: {st.session_state.most_recently_chosen_token}")
 
     # if "counter" not in st.session_state:
-    if session_state.app_state == 'initial_setup_started':
+    if st.session_state.app_state == 'initial_setup_started':
         st.header("BEGINNING OF INITIAL SETUP")
 
         if "starting_bags_created" not in st.session_state:
             tokens.create_starting_bags_of_tokens() # at this moment there should be 2 lists of tokens: 18 WEAKNESS tokens in WEAKNESS_TOKENS_BAG and 18 tokens in TRAIL_TOKENS_BAG, both bags are already randomized/shuffled
             st.session_state.starting_bags_created = True
 
-
         # manually get one token for FOREST, WATER, MOUNTAIN to place initial monsters
         # remove those tokens from the TRAIL_TOKENS_BAG
-
         with st.form('Inputs'):
-            t1 = st.selectbox("Choose token for FOREST monster", tokens.TRAIL_TOKENS_BAG, index=0, key = 't1_selectbox')
-            t2 = st.selectbox("Choose token for FOREST monster", tokens.TRAIL_TOKENS_BAG, index=1, key = 't2_selectbox')
-            t3 = st.selectbox("Choose token for FOREST monster", tokens.TRAIL_TOKENS_BAG, index=2, key = 't3_selectbox')
+            st.subheader('Choose 3 :material/travel_explore: TRAIL TOKENS for initial monsters. They will be removed from corresponding bag.')
+            forest_trail_tokens_list = list(filter(lambda token_names: 'FOREST' in token_names.token_fullname , tokens.TRAIL_TOKENS_BAG))
+            forest_trail_tokens_list.sort(key = sort_tokens_alphabetically)
 
-            if st.form_submit_button('Submit'):
+            water_trail_tokens_list = list(filter(lambda token_names: 'WATER' in token_names.token_fullname , tokens.TRAIL_TOKENS_BAG))
+            water_trail_tokens_list.sort(key = sort_tokens_alphabetically)
+
+            mountain_trail_tokens_list = list(filter(lambda token_names: 'MOUNTAIN' in token_names.token_fullname , tokens.TRAIL_TOKENS_BAG))
+            mountain_trail_tokens_list.sort(key = sort_tokens_alphabetically)
+
+            t1 = st.selectbox("Choose token for :green[FOREST] monster", forest_trail_tokens_list, index=0, key = 't1_selectbox')
+            t2 = st.selectbox("Choose token for :blue[WATER] monster", water_trail_tokens_list, index=0, key = 't2_selectbox')
+            t3 = st.selectbox("Choose token for :grey[MOUNTAIN] monster", mountain_trail_tokens_list, index=0, key = 't3_selectbox')
+
+            submitter = st.form_submit_button('Submit')
+
+            if submitter: #st.form_submit_button('Submit'):
                 tokens.intentionally_remove_one_token_from_bag('TRAIL', t1)
                 tokens.intentionally_remove_one_token_from_bag('TRAIL', t2)
                 tokens.intentionally_remove_one_token_from_bag('TRAIL', t3)
+                
+                #clear some keys from session_state
+                del st.session_state.t1_selectbox
+                del st.session_state.t2_selectbox
+                del st.session_state.t3_selectbox
 
-                st.write('*** 3 STARTING TOKENS FOR MONSTERS WERE REMOVED FROM TOKENS BAG AND WERE PLACED ON THE BOARD***')
-                print('*** 3 STARTING TOKENS FOR MONSTERS WERE REMOVED FROM TOKENS BAG AND WERE PLACED ON THE BOARD***')
+                st.write('*** 3 STARTING :material/travel_explore: TRAIL TOKENS FOR MONSTERS WERE REMOVED FROM TRAIL TOKENS BAG AND WERE PLACED ON THE BOARD***')
+                print('*** 3 STARTING TOKENS FOR MONSTERS WERE REMOVED FROM TRAIL TOKENS BAG AND WERE PLACED ON THE BOARD***')
                 print()
 
-                session_state.app_state = 'monster_trail_tokens_placed'
+                st.session_state.app_state = 'monster_trail_tokens_placed'
 
         # tokens.intentionally_remove_one_token_from_bag('TRAIL', 'TRAIL_FOREST_10')
         # my_bar.progress(percent_complete + 1 * progress_increment, text=progress_text)
@@ -93,15 +122,15 @@ if __name__ == "__main__":
 
         
 
-    if session_state.app_state == 'monster_trail_tokens_placed':
+    if st.session_state.app_state == 'monster_trail_tokens_placed' and 'initial_weakness_tokens_removed' not in st.session_state:
 # now randomly choose 3 weakness tokens and randomly choose their locations by picking 3 TRAIL tokens that will be immediately returned to TRAIL bag later
     # FOREST MONSTER SETUP
         weakness_token = tokens.randomly_remove_one_token_from_bag('WEAKNESS', 'FOREST')
         trail_token = tokens.randomly_remove_one_token_from_bag('TRAIL', 'FOREST')
         color = get_color_for_token(weakness_token)
         print(f"*** PLACE {weakness_token} AT SPOT {trail_token}")
-        st.write(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
-        st.toast(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
+        st.write(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
+        st.toast(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
         # return TRAIL FOREST token to bag
         tokens.return_token_to_bag(trail_token)
         print('-------------------------------------------------')
@@ -111,8 +140,8 @@ if __name__ == "__main__":
         trail_token = tokens.randomly_remove_one_token_from_bag('TRAIL', 'WATER')
         color = get_color_for_token(weakness_token)
         print(f"*** PLACE {weakness_token} AT SPOT {trail_token}")
-        st.write(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
-        st.toast(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
+        st.write(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
+        st.toast(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
         # return TRAIL WATER token to bag
         tokens.return_token_to_bag(trail_token)
         print('-------------------------------------------------')
@@ -122,8 +151,8 @@ if __name__ == "__main__":
         trail_token = tokens.randomly_remove_one_token_from_bag('TRAIL', 'MOUNTAIN')
         color = get_color_for_token(weakness_token)
         print(f"*** PLACE {weakness_token} AT SPOT {trail_token}")
-        st.write(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
-        st.toast(f"*** PLACE :{color}[{weakness_token}] AT SPOT :{color}[{trail_token}]")
+        st.write(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
+        st.toast(f"*** PLACE :{color}[:material/wounds_injuries: {weakness_token}] AT SPOT :{color}[:material/travel_explore: {trail_token}]")
         # return TRAIL MOUNTAIN token to bag
         tokens.return_token_to_bag(trail_token)
         print('-------------------------------------------------')
@@ -140,23 +169,24 @@ if __name__ == "__main__":
         if 'REMOVED_TRAIL_TOKENS_BAG' not in st.session_state:
             st.session_state['REMOVED_TRAIL_TOKENS_BAG'] = tokens.REMOVED_TRAIL_TOKENS_BAG
 
-        if 'most_recently_chosen_token' not in st.session_state:
-            st.session_state['most_recently_chosen_token'] = 'None'
-
         if 'logs_list' not in st.session_state:
             st.session_state['logs_list'] = []
 
-        st.write(st.session_state.app_state)
-        if st.button("Weakness tokens placed. Continue to next step"):
-            session_state.app_state = 'monster_weakness_tokens_placed'
+        
+        #! WTF DOES THIS BUTTON CHANGE APP_STATE WITHOUT EVEN BEING CLICKED ON?
+        st.button("Weakness tokens placed. Continue to next step", key = 'weakness_tokens_placed', on_click= set_app_state_to_monster_weakness_tokens_placed)
+
+        st.session_state.initial_weakness_tokens_removed = True
+
+        if st.session_state.app_state == 'monster_weakness_tokens_placed':
+            # st.session_state.app_state = 'monster_weakness_tokens_placed'
             st.write(st.session_state.app_state)
             st.rerun()
-            st.write(st.session_state.app_state)
 
     # print(tokens.WEAKNESS_TOKENS_BAG)
     # print(tokens.TRAIL_TOKENS_BAG)
 
-    if session_state.app_state == 'monster_weakness_tokens_placed':
+    if st.session_state.app_state == 'monster_weakness_tokens_placed':
         print("*************************************************")
         
         st.session_state.counter += 1
@@ -165,45 +195,61 @@ if __name__ == "__main__":
         
         a, b = st.columns(2)
         c, d = st.columns(2)
-        a.metric(label = "\\# of tokens left in WEAKNESS_TOKENS_BAG", value = len(st.session_state['WEAKNESS_TOKENS_BAG']))
-        b.metric(label = "\\# of tokens removed from WEAKNESS_TOKENS_BAG", value = len(st.session_state['REMOVED_WEAKNESS_TOKENS_BAG']))
-        c.metric(label = "\\# of tokens left in TRAIL_TOKENS_BAG", value = len(st.session_state['TRAIL_TOKENS_BAG']))
-        d.metric(label = "\\# of tokens removed from TRAIL_TOKENS_BAG", value = len(st.session_state['REMOVED_TRAIL_TOKENS_BAG']))
+        a.metric(label = "\\# of tokens left in :material/travel_explore: TRAIL_TOKENS_BAG", value = len(st.session_state['TRAIL_TOKENS_BAG']))
+        b.metric(label = "\\# of tokens removed from :material/travel_explore: TRAIL_TOKENS_BAG", value = len(st.session_state['REMOVED_TRAIL_TOKENS_BAG']))
+        c.metric(label = "\\# of tokens left in :material/wounds_injuries: WEAKNESS_TOKENS_BAG", value = len(st.session_state['WEAKNESS_TOKENS_BAG']))
+        d.metric(label = "\\# of tokens removed from :material/wounds_injuries: WEAKNESS_TOKENS_BAG", value = len(st.session_state['REMOVED_WEAKNESS_TOKENS_BAG']))
         
-        #After initial setup it is time to allow users to click buttons
-        #TODO: randomly_remove_one_token_from_bag in case of quests, new monsters ETC
-        #TODO: return_token_to_bag in case of finished quests, defeated monsters ETC
-
-        st.subheader('REMOVE TOKEN FROM BAG', divider = "green")
+#? After initial setup it is time to allow users to click buttons
+        #? DONE: randomly_remove_one_token_from_bag in case of quests, new monsters ETC
+        st.subheader('DRAW RANDOM TOKEN FROM BAG', divider = "green")
         col1, col2, col3, col4 = st.columns(4, vertical_alignment = 'center')
         with col1:
-            remove_type = st.radio("Choose token type for randomly selected token:", ('TRAIL', 'WEAKNESS'))
+            remove_type = st.radio("Choose token type:", (':material/travel_explore: TRAIL', ':material/wounds_injuries: WEAKNESS'), key = 'remove_type_radio')
+
         with col2:
             remove_territory_type = st.radio("Choose territory type:", ('FOREST', 'WATER', 'MOUNTAIN'))
+        
         with col3:
-            randomly_remove_one_token_from_bag = st.button('Randomly get token from corresponding bag')
+            #?DONE: disable this button if number of remove_type x remove_territory_type tokens in bag is 0
+            # st.write(check_number_of_tokens_left_in_bag(remove_type, remove_territory_type))
+            disabled = check_number_of_tokens_left_in_bag(remove_type, remove_territory_type) <= 0
+            color = get_color_for_token(remove_territory_type)
+            randomly_remove_one_token_from_bag = st.button(f'Randomly draw :{color}[{remove_type} {remove_territory_type}] token from corresponding bag', disabled = disabled)
         if randomly_remove_one_token_from_bag:
             most_recently_chosen_token = tokens.randomly_remove_one_token_from_bag(remove_type, remove_territory_type)
             st.session_state['most_recently_chosen_token'] = most_recently_chosen_token
             st.rerun()
-        with col4:
-            try:
-                st.write(f"Most recently chosen token: {st.session_state.most_recently_chosen_token}")
-            except NameError:
-                st.write("No token was removed yet")
 
-        st.subheader("RETURN TOKEN TO BAG", divider = "green")
+        with col4:
+            if st.session_state.most_recently_chosen_token == 'None':
+                st.write("No token was removed yet")
+            else:
+                color = get_color_for_token(st.session_state.most_recently_chosen_token)
+                if 'WEAKNESS' in st.session_state.most_recently_chosen_token:
+                    icon = ':material/wounds_injuries:'
+                else:
+                    icon = ':material/travel_explore:'
+                st.write(f"Most recently chosen token: :{color}[{icon} {st.session_state.most_recently_chosen_token}]")
+
+
+        #? DONE: return_token_to_bag in case of finished quests, defeated monsters ETC
+        st.subheader("RETURN SELECTED TOKEN TO BAG", divider = "green")
         col1, col2, col3 = st.columns(3, vertical_alignment = 'bottom')
+
         with col1:
-            return_type = st.radio("Choose token type for token to be returned:", ('TRAIL', 'WEAKNESS'))
-            if return_type == 'WEAKNESS':
+            return_type = st.radio("Choose token type:", (':material/travel_explore: TRAIL', ':material/wounds_injuries: WEAKNESS'), key = 'return_type_radio')
+            if 'WEAKNESS' in return_type:
                 tokens_bag = tokens.REMOVED_WEAKNESS_TOKENS_BAG #TODO: figure out how to sort this list
+                icon = ':material/wounds_injuries:'
             else:
                 tokens_bag = tokens.REMOVED_TRAIL_TOKENS_BAG #TODO: figure out how to sort this list
+                icon = ':material/travel_explore:'
+
         with col2:
             token_to_return = st.selectbox(
-                "Choose token to return",
-                tokens_bag,
+                "Choose token to return:",
+                tokens_bag, #.sort(key = sort_tokens_alphabetically),
                 index=0
             )
         with col3:
@@ -215,7 +261,7 @@ if __name__ == "__main__":
         if return_token_to_bag:
             tokens.return_token_to_bag(token_to_return)
             color = get_color_for_token(token_to_return)
-            st.toast(f"Token :{color}[{token_to_return}] has been returned to corresponding bag.", icon=":material/info:")
+            st.toast(f"Token {icon} :{color}[{token_to_return}] has been returned to corresponding bag.", icon=":material/info:")
             st.rerun()
 
         st.button("Run it again")
@@ -223,6 +269,7 @@ if __name__ == "__main__":
         quit()
 
     # debug
+        print("DEBUG AT END OF MAIN.PY")
         token = tokens.Token('TRAIL', 'WATER', 1)
         token.print_definition()
         
