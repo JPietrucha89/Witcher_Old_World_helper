ac09d5c3bcaf5609485907a81f3288e05543ccec
Jakub Pietrucha <elpietruch@gmail.com>
2025-01-04 14:32:07 +0100

    add tokens.py with functions

--- tokens.py
+++ tokens.py
@@ -1,4 +1,5 @@
 import random
+import streamlit as st
 
 '''
 Create class for tokens
@@ -17,11 +18,17 @@ WEAKNESS
 
 '''
 
-global TRAIL_TOKENS_BAG
-global WEAKNESS_TOKENS_BAG
+# global TRAIL_TOKENS_BAG
+# global WEAKNESS_TOKENS_BAG
+# global REMOVED_TRAIL_TOKENS_BAG
+# global REMOVED_WEAKNESS_TOKENS_BAG
+# global most_recently_chosen_token
 
 TRAIL_TOKENS_BAG = []
 WEAKNESS_TOKENS_BAG = []
+REMOVED_TRAIL_TOKENS_BAG = []
+REMOVED_WEAKNESS_TOKENS_BAG = []
+most_recently_chosen_token = ''
 
 class Token:
     def __init__(self, type, territory_type, token_id, is_being_used = False, token_fullname = '' ):
@@ -39,6 +46,11 @@ class Token:
         print(f"Token full name: {self.token_fullname}")
         #print(self.type, self.territory_type, self.token_id, self.is_being_used, self.token_name)
 
+    def __repr__(self):  # Used in lists, debugging, and when you type the object name in the console
+        return f"TokenClass(type='{self.type}', territory_type={self.territory_type}, token_id={self.token_id}, is_being_used={self.is_being_used}, token_fullname={self.token_fullname})"
+
+    def __str__(self):  # Used in print() and str() calls
+        return f"{self.token_fullname}"
 class Bag_of_tokens:
      def __init__(self, type):
           pass
@@ -73,26 +85,85 @@ def create_starting_bags_of_tokens():
     
     random.shuffle(WEAKNESS_TOKENS_BAG)
     random.shuffle(TRAIL_TOKENS_BAG)
+
+    st.write("*** TOKENS BAGS CREATED ***")
+    print("*** TOKENS BAGS CREATED ***")
+    # st.session_state['WEAKNESS_TOKENS_BAG'] = WEAKNESS_TOKENS_BAG
+    # st.session_state['TRAIL_TOKENS_BAG'] = TRAIL_TOKENS_BAG
     # print(WEAKNESS_TOKENS_BAG)
     # print(TRAIL_TOKENS_BAG)
 
 
 def randomly_remove_one_token_from_bag(type, territory_type):
-     pass
-
-def intentionally_remove_one_token_from_bag(type, territory_type, token_fullname):
     if type == 'WEAKNESS':
-        list_to_search = WEAKNESS_TOKENS_BAG
+        list_to_remove_from = WEAKNESS_TOKENS_BAG
+        list_to_add_to = REMOVED_WEAKNESS_TOKENS_BAG
     elif type == 'TRAIL':
-        list_to_search = TRAIL_TOKENS_BAG
+        list_to_remove_from = TRAIL_TOKENS_BAG
+        list_to_add_to = REMOVED_TRAIL_TOKENS_BAG
 
-    list_to_search.remove()
+    random.shuffle(list_to_remove_from)
+
+    # iterate through the list of tokens and find the first token that matches the territory type
+    for token in list_to_remove_from:
+        if token.territory_type == territory_type:
+    # remove it from the list and add it to the list of removed tokens
+            print("RANDOMLY REMOVED TOKEN: " + token.token_fullname)
+            list_to_add_to.append(token)
+            list_to_remove_from.remove(token)
+            most_recently_chosen_token = token.token_fullname
+            return most_recently_chosen_token
+
+    pass
+
+def intentionally_remove_one_token_from_bag(type, token_fullname):
+
+    if isinstance(token_fullname, str) and '_' in token_fullname:
+        token_type = token_fullname.split('_')[0]
+    else: # TODO: how to convert names from st.selectbox to token names
+        token_type = token_fullname.type
+        token_fullname = token_fullname.token_fullname
+
+    if type == 'WEAKNESS':
+        list_to_remove_from = WEAKNESS_TOKENS_BAG
+        list_to_add_to = REMOVED_WEAKNESS_TOKENS_BAG
+    elif type == 'TRAIL':
+        list_to_remove_from = TRAIL_TOKENS_BAG
+        list_to_add_to = REMOVED_TRAIL_TOKENS_BAG
+
+    for token in list_to_remove_from:
+        if token.token_fullname == token_fullname:
+            print("INTENTIONALLY REMOVED TOKEN: " + token.token_fullname)
+            list_to_add_to.append(token)
+            list_to_remove_from.remove(token)
+            most_recently_chosen_token = token.token_fullname
+            return most_recently_chosen_token
+    
+    print(f"Failed to find and remove given token: {token_fullname}")
 
 def return_token_to_bag(token_name):
-    if type == 'WEAKNESS':
-        list_to_add = WEAKNESS_TOKENS_BAG
-    elif type == 'TRAIL':
-        list_to_add = TRAIL_TOKENS_BAG
+    
+    if isinstance(token_name, str) and '_' in token_name:
+        token_type = token_name.split('_')[0]
+    else: # TODO: how to convert names from st.selectbox to token names
+        token_type = token_name.type
+        token_name = token_name.token_fullname
+
+    if token_type == 'WEAKNESS':
+        list_to_remove_from = REMOVED_WEAKNESS_TOKENS_BAG
+        list_to_add_to = WEAKNESS_TOKENS_BAG
+    elif token_type == 'TRAIL':
+        list_to_remove_from = REMOVED_TRAIL_TOKENS_BAG
+        list_to_add_to = TRAIL_TOKENS_BAG
+
+    for token in list_to_remove_from:
+        if token.token_fullname == token_name:
+            print(f"{token_type} TOKEN RETURNED TO CORRESPONDING BAG: " + token.token_fullname)
+            list_to_add_to.append(token)
+            list_to_remove_from.remove(token)
+            return
+    
+    print(f"Failed to return given token: {token_name}")
 
 # MAIN
 # create_starting_bags_of_tokens()
