4f1233cac4f22e833598db1cbd4718bd31f8b919
Jakub Pietrucha <elpietruch@gmail.com>
2025-01-07 22:55:41 +0100

    fix: proper tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,8 +3,8 @@ from main import get_color_for_token
 
 def test_get_color_for_token():
     assert get_color_for_token('TRAIL_FOREST_') == 'green'
-    assert get_color_for_token('TRAIL_WATER_') == 'grey'
-    assert get_color_for_token('TRAIL_MOUNTAIN') == 'blue'
+    assert get_color_for_token('TRAIL_WATER_') == 'blue'
+    assert get_color_for_token('TRAIL_MOUNTAIN') == 'grey'
     assert get_color_for_token('WEAKNESS_FOREST_') == 'green'
-    assert get_color_for_token('WEAKNESS_WATER_') == 'grey'
-    assert get_color_for_token('WEAKNESS_MOUNTAIN') == 'blue'
+    assert get_color_for_token('WEAKNESS_WATER_') == 'blue'
+    assert get_color_for_token('WEAKNESS_MOUNTAIN') == 'grey'
